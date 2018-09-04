eade05a9dc202e7e4fcb4a1209bf41937543d666
Diego Buenrostro <42652866+TheAce805@users.noreply.github.com>
2018-09-04 10:47:19 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
      $("h1").click(function() {
        $("p").css({"color":"orange", "background-color":"lightblue"});
+          $("img").css()"border": "2px solid red"));
      })
 })
